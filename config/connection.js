d4e5d90628e1b1f760843d6fe58afe669162e9a8
Shinichi <miyakawashi@gmail.com>
2022-01-31 18:43:47 -0500

    bugs found in config/connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // import Sequelize constructor from the library
-let sequelize = require('sequelize');
-require('dotenv').config();
+let sequelize;
+// require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -11,11 +11,5 @@ if (process.env.JAWSDB_URL) {
         port: 3306
     });
 }
-// create connection to DB, pass sql info for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
 
 module.exports = sequelize;
