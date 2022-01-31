dc2f67dbd401c125c8ca72b5d4514ca26e031970
Shinichi <miyakawashi@gmail.com>
2022-01-31 18:56:15 -0500

    config/connection.js error fixes

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,6 @@ require('dotenv').config();
 // create connection to DB, pass sql info for username and password
 let sequelize 
 
-// new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
-
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
