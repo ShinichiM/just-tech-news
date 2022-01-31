a8d2e2f05491fc1b8a292c43b85ffe16c4f43636
Shinichi <miyakawashi@gmail.com>
2022-01-31 18:53:24 -0500

    new fix for connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 // import Sequelize constructor from the library
-let sequelize;
-// require('dotenv').config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// create connection to DB, pass sql info for username and password
+let sequelize 
+
+// new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
